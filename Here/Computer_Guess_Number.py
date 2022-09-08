f431f2a8f13bde90ab285261dfa2d3657687d3ee
Matin Mohamadi <90261350+irealmatin@users.noreply.github.com>
2022-09-08 15:37:40 +0430

    second one was created

---
+++ Here/Computer_Guess_Number.py
@@ -0,0 +1,20 @@
+# first import randint for Return a random integer N such that a <= N <= b. Alias for randrange(a, b+1).
+from random import randint
+
+#make a function for write in OOP form
+def computer_guess(x):
+    min_g = 1
+    max_g = x
+    result = ""
+    while result != 'c':
+        if min_g != max_g : guess = randint(min_g,max_g) # run when guess is not 1
+        else : guess = min_g # if answer is 1 . x = y ,  1 = 1
+        result = input(f"is {guess} too high(h) , too low(l) or correct(c) ?  ").lower()
+        if result == 'h':
+            max_g = guess - 1
+        if result == 'l':
+            min_g = guess + 1
+    
+    print(f"wow . I can guessed the number {guess} correctly")
+
+computer_guess(20) # optional value
