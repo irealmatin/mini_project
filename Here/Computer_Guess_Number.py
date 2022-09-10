35dfddf6c042e80d1e36e535e82d7f34688b8563
Matin Mohamadi <90261350+irealmatin@users.noreply.github.com>
2022-09-10 16:54:16 +0430

    update script

--- Here/Computer_Guess_Number.py
+++ Here/Computer_Guess_Number.py
@@ -3,14 +3,14 @@ from random import randint
 
 #make a function for write in OOP form
 def computer_guess(x):
-    min_g = 1
-    max_g = x
-    result = ""
+    min_g = 1 # Lower number than can choose
+    max_g = x # last number
+    result = "" 
     while result != 'c':
         if min_g != max_g : guess = randint(min_g,max_g) # run when guess is not 1
         else : guess = min_g # if answer is 1 . x = y ,  1 = 1
         result = input(f"is {guess} too high(h) , too low(l) or correct(c) ?  ").lower()
-        if result == 'h':
+        if result == 'h': 
             max_g = guess - 1
         if result == 'l':
             min_g = guess + 1
