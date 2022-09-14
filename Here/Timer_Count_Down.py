ac64193024b097aa5e8c8150f1b1fd68b846ada2
Matin Mohamadi <90261350+irealmatin@users.noreply.github.com>
2022-09-14 15:18:46 +0430

    Update Timer_Count_Down.py

--- Here/Timer_Count_Down.py
+++ Here/Timer_Count_Down.py
@@ -5,8 +5,8 @@ def count_down(tm):
     this function get a time in seconds form and count down that numbar in a form like  this -> 00:00
     """
     while tm :
-        minutes , seconds = divmod(tm,60)
-        timer = '{:02d}:{:02d}'.format(minutes,seconds)
+        minutes , seconds = divmod(tm,60) # this give us a tuple with 2 index. first one min second one secs
+        timer = '{:02d}:{:02d}'.format(minutes,seconds) 
         print(timer , end = '\r')
         time.sleep(1)
         tm -= 1
