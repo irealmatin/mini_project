69aa6ed01a5dfc808989bb64ba2a234224193488
Matin Mohamadi <90261350+irealmatin@users.noreply.github.com>
2022-09-14 16:27:39 +0430

    add password generator

---
+++ Here/Password_Generator.py
@@ -0,0 +1,19 @@
+import random
+
+print("Welcome:))\nthis is Password generator program")
+
+char_pass = 'abcdefghijklmnopqrstuvwxyzABCDEFGHILKLMNOPQRSTUVWXYZ0123456789~`!@#$%^&*()-_+=}{[]|\;:"<>,./?'
+
+print("******\nAnswer the question below for make your password ...")
+
+number = int(input("1 - How many password do you want :  "))
+lenght = int(input("2 - input your password lenght :  "))
+
+print("\nYour strong passwords are ready........")
+
+for c in range(number):
+    password = ''
+    for n in range(lenght):
+        password +=random.choice(char_pass)
+
+    print(password)
