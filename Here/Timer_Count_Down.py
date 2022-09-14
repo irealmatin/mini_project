473444ad96887e3b06d6d557785075c46a1d9b65
Matin Mohamadi <90261350+irealmatin@users.noreply.github.com>
2022-09-14 15:16:23 +0430

    add timer_down

---
+++ Here/Timer_Count_Down.py
@@ -0,0 +1,17 @@
+import time
+
+def count_down(tm):
+    """
+    this function get a time in seconds form and count down that numbar in a form like  this -> 00:00
+    """
+    while tm :
+        minutes , seconds = divmod(tm,60)
+        timer = '{:02d}:{:02d}'.format(minutes,seconds)
+        print(timer , end = '\r')
+        time.sleep(1)
+        tm -= 1
+
+    print("Time is over!!")
+
+inp = int(input("write the time in second : "))
+count_down(inp)
