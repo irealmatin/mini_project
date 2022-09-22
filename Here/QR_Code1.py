23c7a7449ea06fbc4277dd21d3ab344eabc07624
Matin Mohamadi <90261350+irealmatin@users.noreply.github.com>
2022-09-22 11:31:19 +0330

    Update QR_Code1.py

--- Here/QR_Code1.py
+++ Here/QR_Code1.py
@@ -1,8 +1,9 @@
 import qrcode # you should import this in your terminal 
 
-my_data = 'https://github.com/irealmatin'
+my_data = 'https://github.com/irealmatin' # Optional data for make a QR code 
 
-the_qr = qrcode.QRCode(
+the_qr = qrcode.QRCode( 
+    # customize your QR code here
     version=1,
     box_size=10,
     border=4
@@ -10,6 +11,6 @@ the_qr = qrcode.QRCode(
 the_qr.add_data(my_data)
 the_qr.make(fit = True)
  
-img = the_qr.make_image(fill_color = "blue", back_color = "white")
+img = the_qr.make_image(fill_color = "blue", back_color = "white") 
 
-img.save('C:/Users/ireal/OneDrive/دسکتاپ/Here/qr.png')
+img.save('C:/Users/ireal/OneDrive/دسکتاپ/Here/qr.png') # optional directory
